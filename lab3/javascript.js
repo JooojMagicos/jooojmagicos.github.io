fcaeeb94777a1af4fa2160c6a1dab5a7f34d076a
JoojMagicos <josejosejose2348@gmail.com>
2025-09-26 15:53:01 +0100

    nesse comit existem diversas alteracoes dentro do multimedia

---
+++ lab3/javascript.js
@@ -0,0 +1,8 @@
+
+  const menuBtn = document.getElementById('menu-btn');
+  const navUl = document.querySelector('nav ul');
+
+  menuBtn.addEventListener('click', () => {
+    navUl.classList.toggle('show');
+  });
+
